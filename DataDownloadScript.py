1cf480cf683509efc8c26c162836c48c6902f64d
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-11-22 00:17:15 -0700

    First part of URLbuilder started
    I name her URLATRON

--- DataDownloadScript.py
+++ DataDownloadScript.py
@@ -1,7 +1,20 @@
 #Code that generates URL for Factfinder census data and pastes it to clipboard
 
+#URL-BUILDER COMMENCE:
+standardURL = 'http://factfinder2.census.gov/bkmk/table/1.0/en/DEC/10_SF1/'
+
+#Get the data level
+state = '040'
+county = '050'
+censustr = '140'
+zipcode = '860'
+
+level = state
+
+geoid = level + '0000US' + fips
+
 #This code pastes a string to your clipboard, regardless of OS.
-#Codes straight outta StackOverflow:
+#Code straight outta StackOverflow.
 
 #Windows way:
 output = 'TEST AAAHHH'
@@ -12,6 +25,7 @@ r.clipboard_clear()
 r.clipboard_append(output)
 r.destroy()
 
+#Mac way
 import subprocess
 process = subprocess.Popen(
         'pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
